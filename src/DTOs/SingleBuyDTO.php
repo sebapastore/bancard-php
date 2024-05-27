9fb72a6a842b2b4405c5bfde763fd92b76586d1c
sebapastore <sebastianerp@gmail.com>
2024-05-27 18:14:57 -0400

    Add DTOs

---
+++ src/DTOs/SingleBuyDTO.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SebaPastore\BancardPHP\DTOs;
+
+class SingleBuyDTO
+{
+    public function __construct(
+        readonly int $amount,
+        readonly int $shopProcessId,
+        readonly string $description,
+        readonly string $returnUrl,
+        readonly string|null $additionalData = null,
+        readonly string $currency = 'PYG',
+        readonly string|null $cancelUrl = null
+    )
+    {
+    }
+
+}
--- src/Resources/SingleBuy.php
+++ src/Resources/SingleBuy.php
@@ -3,10 +3,16 @@
 namespace SebaPastore\BancardPHP\Resources;
 
 use SebaPastore\BancardPHP\Contracts\Resources\SingleBuyContract;
+use SebaPastore\BancardPHP\DTOs\SingleBuyDTO;
 use SebaPastore\BancardPHP\Resources\Concerns\Signable;
 
 class SingleBuy implements SingleBuyContract
 {
     use Signable;
+
+    public function create(SingleBuyDTO $data)
+    {
+
+    }
 
 }
