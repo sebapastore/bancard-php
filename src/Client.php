454d755398aea94028f8cd26ebe6b829ad8a6fe6
sebapastore <sebastianerp@gmail.com>
2024-05-27 19:37:52 -0400

    add http to client

--- src/Client.php
+++ src/Client.php
@@ -3,17 +3,21 @@
 namespace SebaPastore\BancardPHP;
 
 use SebaPastore\BancardPHP\Contracts\ClientContract;
+use SebaPastore\BancardPHP\Contracts\HttpClientContract;
 use SebaPastore\BancardPHP\Contracts\KeyManagerContract;
 use SebaPastore\BancardPHP\Resources\SingleBuy;
 
 class Client implements ClientContract
 {
-    public function __construct(private readonly KeyManagerContract $keyManager)
+    public function __construct(
+        private readonly HttpClientContract $httpClient,
+        private readonly KeyManagerContract $keyManager
+    )
     {
     }
 
     public function singleBuy(): SingleBuy
     {
-        return new SingleBuy($this->keyManager);
+        return new SingleBuy($this->httpClient, $this->keyManager);
     }
 }
